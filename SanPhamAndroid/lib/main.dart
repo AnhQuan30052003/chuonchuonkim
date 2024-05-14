3373fc3308338154a8bb6dbaac2cfa83785a73cc
Admin <anhquan300503@gmail.com>
2024-05-14 19:10:29 +0700

    xoá code thừa trong file main.dart

--- SanPhamAndroid/lib/main.dart
+++ SanPhamAndroid/lib/main.dart
@@ -4,41 +4,8 @@ import 'package:get/get.dart';
 import 'pages/pageHome.dart';
 import 'pages/pageCart.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const ChuonChuonKimApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      home: ChuonChuonKimApp(),
-    );
-  }
-}
-
-// Chạy thử chương trình đơn giản
-class PageDemo extends StatelessWidget {
-  const PageDemo({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Title demo"),
-      ),
-      body: const Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [Text("Run file demo successful")],
-      )),
-    );
-  }
-}
-
-// App chính, sau này code FirebaseConnect cho class này, thay MyApp()
 class ChuonChuonKimApp extends StatelessWidget {
   const ChuonChuonKimApp({super.key});
 
