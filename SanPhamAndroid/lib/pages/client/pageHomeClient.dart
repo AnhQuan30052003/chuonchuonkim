1e21c3bbad87d54601e4a55d5afc5711244e2dcc
tiendatpy <datn71356@gmail.com>
2024-05-22 11:28:58 +0700

    fix circle product

--- SanPhamAndroid/lib/pages/client/pageHomeClient.dart
+++ SanPhamAndroid/lib/pages/client/pageHomeClient.dart
@@ -62,211 +62,207 @@ class PageHomeClient extends StatelessWidget {
             padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
             child: SingleChildScrollView(
               scrollDirection: Axis.vertical,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // * search
-                  SizedBox(
-                    height: 50,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10),
-                        border: OutlineInputBorder(
+              child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                // * search
+                SizedBox(
+                  height: 50,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      prefixIcon: const Icon(
+                        Icons.search,
+                        color: Colors.redAccent,
+                      ),
+                      suffixIcon: const Icon(Icons.sort, color: Colors.redAccent),
+                      hintText: "Tìm kiếm",
+                    ),
+                    onChanged: (input) {},
+                  ),
+                ),
+                // * end search
+
+                const SizedBox(height: 10),
+
+                // * filter
+                SizedBox(
+                  height: 70,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: controller.listProductType.length,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      var item = controller.listProductType[index];
+                      return Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                        height: 60,
+                        width: 60,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
                           borderRadius: BorderRadius.circular(10),
-                        ),
-                        prefixIcon: const Icon(
-                          Icons.search,
-                          color: Colors.redAccent,
-                        ),
-                        suffixIcon: const Icon(Icons.sort, color: Colors.redAccent),
-                        hintText: "Tìm kiếm",
-                      ),
-                      onChanged: (input) {
-
-                      },
-                    ),
-                  ),
-                  // * end search
-
-                  const SizedBox(height: 10),
-
-                  // * filter
-                  SizedBox(
-                    height: 70,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: controller.listProductType.length,
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        var item = controller.listProductType[index];
-                        return Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-                          height: 60,
-                          width: 60,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: const [
-                              BoxShadow(
-                                color: Colors.black12,
-                                spreadRadius: 2,
-                                blurRadius: 4,
-                              ),
-                            ],
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(10),
-                            child: GestureDetector(
-                              child: Image.network(item.hinhAnhLSP),
-                              onTap: () {
-                                // controller.showProductType(idLSP: list[index].maLSP);
-                              },
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.black12,
+                              spreadRadius: 2,
+                              blurRadius: 4,
                             ),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: GestureDetector(
+                            child: Image.network(item.hinhAnhLSP),
+                            onTap: () {
+                              // controller.showProductType(idLSP: list[index].maLSP);
+                            },
                           ),
-                        );
-                      },
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                // * end filter
+
+                // * Phổ biến
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Phổ biến",
+                      style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  // * end filter
-
-                  // * Phổ biến
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        "Phổ biến",
-                        style: TextStyle(
-                          color: Colors.black87,
-                          fontSize: 17,
-                          fontWeight: FontWeight.bold,
-                        ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: const Text(
+                        "Tất cả",
+                        style: TextStyle(color: Colors.redAccent, fontSize: 15),
                       ),
-                      GestureDetector(
-                        onTap: () {},
-                        child: const Text(
-                          "Tất cả",
-                          style: TextStyle(color: Colors.redAccent, fontSize: 15),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
+                ),
 
-                  // * card product
-                  SizedBox(
-                    height: 250,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: list.length,
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        var item = list[index];
-                        return Stack(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(top: 8.0),
-                              child: Container(
-                                margin: const EdgeInsets.all(10),
-                                height: 250,
-                                width: 190,
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(10),
-                                  boxShadow: const [
-                                    BoxShadow(
-                                      blurRadius: 4,
-                                      spreadRadius: 2,
-                                      color: Colors.black12,
-                                    ),
-                                  ],
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(10),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    children: [
-                                      const SizedBox(height: 7),
-                                      Text(
-                                        item.tenSP,
-                                        style: const TextStyle(
-                                            color: Colors.black,
-                                            fontSize: 17,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      Text(
-                                        item.moTaSP,
-                                        style: const TextStyle(color: Colors.black45, fontSize: 15),
-                                      ),
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(
-                                            "${item.giaSP}đ",
-                                            style: const TextStyle(fontWeight: FontWeight.normal),
-                                          ),
-                                          Container(
-                                            height: 30,
-                                            width: 30,
-                                            decoration: BoxDecoration(
-                                              color: Colors.black12,
-                                              borderRadius: BorderRadius.circular(30),
-                                            ),
-                                            child: const Icon(Icons.favorite_border_outlined),
-                                          ),
-                                        ],
-                                      )
-                                    ],
+                // * card product
+                SizedBox(
+                  height: 250,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: list.length,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      var item = list[index];
+                      return Stack(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8.0),
+                            child: Container(
+                              margin: const EdgeInsets.all(10),
+                              height: 250,
+                              width: 190,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    blurRadius: 4,
+                                    spreadRadius: 2,
+                                    color: Colors.black12,
                                   ),
-                                ),
+                                ],
                               ),
-                            ),
-                            Positioned(
-                              left: 35,
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(80),
-                                  boxShadow: const [
-                                    BoxShadow(
-                                      blurRadius: 4,
-                                      spreadRadius: 2,
-                                      color: Colors.black12,
+                              child: Padding(
+                                padding: const EdgeInsets.all(10),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    const SizedBox(height: 7),
+                                    Text(
+                                      item.tenSP,
+                                      style: const TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 17,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      item.moTaSP,
+                                      style: const TextStyle(color: Colors.black45, fontSize: 15),
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          "${item.giaSP}đ",
+                                          style: const TextStyle(fontWeight: FontWeight.normal),
+                                        ),
+                                        Container(
+                                          height: 30,
+                                          width: 30,
+                                          decoration: BoxDecoration(
+                                            color: Colors.black12,
+                                            borderRadius: BorderRadius.circular(30),
+                                          ),
+                                          child: const Icon(Icons.favorite_border_outlined),
+                                        ),
+                                      ],
                                     )
                                   ],
                                 ),
-                                child: Image.network(
-                                  item.hinhAnhSP,
-                                  height: 140,
-                                  width: 140,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            left: 45,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(80),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    blurRadius: 4,
+                                    spreadRadius: 4,
+                                    color: Colors.black12,
+                                  )
+                                ],
+                              ),
+                              child: ClipOval(
+                                child: SizedBox.fromSize(
+                                  size: const Size.fromRadius(60), // Image radius
+                                  child: Image.network(item.hinhAnhSP, fit: BoxFit.cover),
                                 ),
                               ),
-                            )
-                          ],
-                        );
-                      },
-                    ),
+                            ),
+                          )
+                        ],
+                      );
+                    },
                   ),
-                  // * end card product
-                  // * end phổ biến
+                ),
+                // * end card product
+                // * end phổ biến
 
-                  // * sản phẩm
-                  const SizedBox(height: 10),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Sản phẩm",
-                        style: TextStyle(
-                          color: Colors.black87,
-                          fontSize: 17,
-                          fontWeight: FontWeight.bold,
-                        ),
+                // * sản phẩm
+                const SizedBox(height: 10),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Sản phẩm",
+                      style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
                       ),
-                    ],
-                  ),
-                  // * end sản phẩm
-                ]
-              ),
+                    ),
+                  ],
+                ),
+                // * end sản phẩm
+              ]),
             ),
           );
         },
