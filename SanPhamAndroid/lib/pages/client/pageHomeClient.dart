a5824b78298ee58ad9f1f54d3bc26009e23f3925
Admin <anhquan300503@gmail.com>
2024-05-26 00:59:03 +0700

    cập nhật: gộp code

--- SanPhamAndroid/lib/pages/client/pageHomeClient.dart
+++ SanPhamAndroid/lib/pages/client/pageHomeClient.dart
@@ -12,6 +12,7 @@ import '../../controllers/chuonChuonKimController.dart';
 import '../../database/connect/firebaseConnect.dart';
 import '../../database/models/Product.dart';
 import '../../helper/widgetClient.dart';
+import '../system/aboutme/account.dart';
 
 class ClientConnect extends StatelessWidget {
   const ClientConnect({super.key});
@@ -84,7 +85,7 @@ class _PageHomeClientState extends State<PageHomeClient> {
       return home();
     }
     if (index == 1) return favorite();
-    return account();
+    return account(context);
   }
 
   Widget home() {
@@ -185,136 +186,6 @@ class _PageHomeClientState extends State<PageHomeClient> {
             space(0, 20),
             buildInstruction(text: "Có thể bạn cũng thích"),
             buildGridViewProducts(context: context, list: c.listProdutsGridView, showNotFound: false),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget account() {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 100,
-              width: 272,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  ClipOval(
-                    child: SizedBox.fromSize(
-                      size: const Size.fromRadius(50), // Image radius
-                      child: Image.network("https://i.pinimg.com/474x/df/ce/a7/dfcea7989195d3273c2bcb367fca0a83.jpg", fit: BoxFit.cover),
-                    ),
-                  ),
-                  const Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text("Tên khách hàng", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
-                        Text("Tài khoản", style: TextStyle(color: Color.fromARGB(96, 27, 10, 10))),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                color: const Color.fromRGBO(246, 248, 250, 1),
-              ),
-              child: ListView(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                children: [
-                  ListTile(
-                    horizontalTitleGap: 10,
-                    title: const Text("Thông tin cá nhân"),
-                    leading: Container(
-                      height: 40,
-                      width: 40,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Colors.white,
-                      ),
-                      child: const Icon(Icons.person_2_outlined),
-                    ),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.arrow_forward_ios_outlined, size: 17),
-                      onPressed: () {},
-                    ),
-                  ),
-                  ListTile(
-                    horizontalTitleGap: 10,
-                    title: const Text("Địa chỉ nhận hàng"),
-                    leading: Container(
-                      height: 40,
-                      width: 40,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Colors.white,
-                      ),
-                      child: const Icon(CupertinoIcons.location_solid),
-                    ),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.arrow_forward_ios_outlined, size: 17),
-                      onPressed: () {},
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              color: const Color.fromRGBO(246, 248, 250, 1),
-              width: MediaQuery.of(context).size.width,
-              child: ListView(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                children: [
-                  ListTile(
-                    horizontalTitleGap: 10,
-                    title: const Text("Đổi mật kẩu"),
-                    leading: Container(
-                      height: 40,
-                      width: 40,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Colors.white,
-                      ),
-                      child: const Icon(Icons.vpn_key),
-                    ),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.arrow_forward_ios_outlined, size: 17),
-                      onPressed: () {},
-                    ),
-                  ),
-                  ListTile(
-                    horizontalTitleGap: 10,
-                    title: const Text("Đăng xuất"),
-                    leading: Container(
-                      height: 40,
-                      width: 40,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Colors.white,
-                      ),
-                      child: const Icon(Icons.person_2_outlined),
-                    ),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.arrow_forward_ios_outlined, size: 17),
-                      onPressed: () {},
-                    ),
-                  ),
-                ],
-              ),
-            )
           ],
         ),
       ),
--- SanPhamAndroid/lib/pages/system/aboutme/account.dart
+++ SanPhamAndroid/lib/pages/system/aboutme/account.dart
@@ -1,146 +1,131 @@
-import 'package:chuonchuonkim_app/pages/system/aboutme/personalInfo.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
-  return runApp(const App());
-}
+import 'personalInfo.dart';
 
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const GetMaterialApp(title: "App", debugShowCheckedModeBanner: false, home: MyAccount());
-  }
-}
-
-class MyAccount extends StatelessWidget {
-  const MyAccount({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("My Account"),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 100,
-                width: 272,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    ClipOval(
-                      child: SizedBox.fromSize(
-                        size: const Size.fromRadius(50), // Image radius
-                        child: Image.network(
-                            "https://i.pinimg.com/474x/df/ce/a7/dfcea7989195d3273c2bcb367fca0a83.jpg",
-                            fit: BoxFit.cover),
-                      ),
-                    ),
-                    const Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text("Tên khách hàng",
-                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
-                          Text("Chữ ký...",
-                              style: TextStyle(color: Color.fromARGB(96, 27, 10, 10))),
-                        ],
-                      ),
-                    ),
-                  ],
+Widget account(BuildContext context) {
+  return SingleChildScrollView(
+    child: Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 100,
+            width: 272,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                ClipOval(
+                  child: SizedBox.fromSize(
+                    size: const Size.fromRadius(50), // Image radius
+                    child: Image.network("https://i.pinimg.com/474x/df/ce/a7/dfcea7989195d3273c2bcb367fca0a83.jpg", fit: BoxFit.cover),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  color: const Color.fromRGBO(246, 248, 250, 1),
+                const Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text("Tên khách hàng",
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+                      Text("Chữ ký...",
+                          style: TextStyle(color: Color.fromARGB(96, 27, 10, 10))),
+                    ],
+                  ),
                 ),
-                child: ListView(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  children: [
-                    ListTile(
-                      horizontalTitleGap: 10,
-                      title: const Text("Thông tin cá nhân"),
-                      leading: Container(
-                        height: 40,
-                        width: 40,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: Colors.white,
-                        ),
-                        child: const Icon(Icons.person_2_outlined, color: Colors.orangeAccent),
-                      ),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.arrow_forward_ios_outlined, size: 17),
-                        onPressed: () {
-                          Get.to(const PersonalInfo());
-                        },
-                      ),
-                    ),
-                    ListTile(
-                      horizontalTitleGap: 10,
-                      title: const Text("Địa chỉ nhận hàng"),
-                      leading: Container(
-                        height: 40,
-                        width: 40,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: Colors.white,
-                        ),
-                        child: const Icon(CupertinoIcons.location_solid, color: Colors.lightBlue),
-                      ),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.arrow_forward_ios_outlined, size: 17),
-                        onPressed: () {},
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 20),
-              Container(
-                color: const Color.fromRGBO(246, 248, 250, 1),
-                width: MediaQuery.of(context).size.width,
-                child: ListView(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  children: [
-                    ListTile(
-                      horizontalTitleGap: 10,
-                      title: const Text("Đăng xuất"),
-                      leading: Container(
-                        height: 40,
-                        width: 40,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: Colors.white,
-                        ),
-                        child: const Icon(Icons.logout_rounded, color: Colors.redAccent),
-                      ),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.arrow_forward_ios_outlined, size: 17),
-                        onPressed: () {},
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+              ],
+            ),
           ),
-        ),
+          const SizedBox(height: 20),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: const Color.fromRGBO(246, 248, 250, 1),
+            ),
+            child: ListView(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              children: [
+                GestureDetector(
+                  child: ListTile(
+                    horizontalTitleGap: 10,
+                    title: const Text("Thông tin cá nhân"),
+                    leading: Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.white,
+                      ),
+                      child: const Icon(Icons.person_2_outlined, color: Colors.orangeAccent),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.arrow_forward_ios_outlined, size: 17),
+                      onPressed: () {
+                        Get.to(const PersonalInfo());
+                      },
+                    ),
+                  ),
+                  onTap: () => Get.to(const PersonalInfo()),
+                ),
+                GestureDetector(
+                  child: ListTile(
+                    horizontalTitleGap: 10,
+                    title: const Text("Địa chỉ nhận hàng"),
+                    leading: Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.white,
+                      ),
+                      child: const Icon(CupertinoIcons.location_solid, color: Colors.lightBlue),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.arrow_forward_ios_outlined, size: 17),
+                      onPressed: () {},
+                    ),
+                  ),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          Container(
+            color: const Color.fromRGBO(246, 248, 250, 1),
+            width: MediaQuery.of(context).size.width,
+            child: ListView(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              children: [
+                GestureDetector(
+                  child: ListTile(
+                    horizontalTitleGap: 10,
+                    title: const Text("Đăng xuất"),
+                    leading: Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.white,
+                      ),
+                      child: const Icon(Icons.logout_rounded, color: Colors.redAccent),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.arrow_forward_ios_outlined, size: 17),
+                      onPressed: () {},
+                    ),
+                  ),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          )
+        ],
       ),
-    );
-  }
+    ),
+  );
 }
