954fe16fd56e66cfc8cdb12299fbb6cc6aa76d1c
Admin <anhquan300503@gmail.com>
2024-05-27 23:32:17 +0700

    cập nhật cấu trúc page notification clinet & admin

--- SanPhamAndroid/lib/helper/widgetClient.dart
+++ SanPhamAndroid/lib/helper/widgetClient.dart
@@ -367,3 +367,125 @@ Widget buildProductsPopulator() {
     },
   );
 }
+
+Padding _buildTinTucThongBao(double spacePading, String text) {
+  return Padding(
+    padding: EdgeInsets.only(left: spacePading),
+    child: buildInstruction(text: text),
+  );
+}
+
+Widget buildStreamBuilderNotification() {
+  return StreamBuilder(
+    stream: NotificationSnapshot.streamData(),
+    builder: (context, snapshot) {
+      if (!snapshot.hasData) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+
+      List<NotificationSnapshot> list = [], listNew = [], listOld = [];
+
+      try {
+        list = snapshot.data!;
+
+        for (NotificationSnapshot l in list) {
+          if (l.notification.seen) {
+            listOld.add(l);
+          } else {
+            listNew.add(l);
+          }
+        }
+
+        listNew.sort((NotificationSnapshot a, NotificationSnapshot b) => (b.notification.idNoti.compareTo(a.notification.idNoti)));
+        listOld.sort((NotificationSnapshot a, NotificationSnapshot b) => (b.notification.idNoti.compareTo(a.notification.idNoti)));
+      } catch (error) {
+        list = [];
+      }
+
+      double spacePading = 5.0;
+      return SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(left: spacePading, right: spacePading),
+          child: Column(
+            children: [
+              _buildTinTucThongBao(spacePading, "Mới(${listNew.length})"),
+              Column(
+                children: listNew.map(
+                  (ns) {
+                    return GestureDetector(
+                      child: Card(
+                        color: Colors.white,
+                        child: SizedBox(
+                          height: 50,
+                          child: Row(
+                            children: [
+                              const SizedBox(
+                                width: 35,
+                                child: Icon(Icons.mark_as_unread, color: Colors.grey, size: 40),
+                              ),
+                              space(10, 0),
+                              SizedBox(
+                                height: 50,
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(ns.notification.text, style: const TextStyle(color: Colors.black))
+                                )
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      onTap: () async {
+                        // Chuyển page..
+                        ns.notification.seen = true;
+                        await ns.update(ns.notification);
+                      },
+                    );
+                  }
+                ).toList(),
+              ),
+
+              _buildTinTucThongBao(spacePading, "Trước đó(${listOld.length})"),
+              Column(
+                children: listOld.map(
+                  (ns) {
+                    return GestureDetector(
+                      child: Card(
+                        color: Colors.black12,
+                        child: SizedBox(
+                          height: 50,
+                          child: Row(
+                            children: [
+                              const SizedBox(
+                                width: 35,
+                                child: Icon(Icons.mark_as_unread, color: Colors.white, size: 40),
+                              ),
+                              space(10, 0),
+                              SizedBox(
+                                height: 50,
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(ns.notification.text, style: const TextStyle(color: Colors.white))
+                                )
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+                        // Chuyển page..
+                        // Get.to();
+                      },
+                    );
+                  }
+                ).toList(),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
