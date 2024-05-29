1f4d2f84721c82c2949c0046c8c5a7d53b712c49
tiendatpy <datn71356@gmail.com>
2024-05-29 21:04:33 +0700

    page login

--- SanPhamAndroid/lib/pages/system/sign/login.dart
+++ SanPhamAndroid/lib/pages/system/sign/login.dart
@@ -12,11 +12,10 @@ class App extends StatelessWidget {
     return const GetMaterialApp(
       title: "App",
       debugShowCheckedModeBanner: false,
-      home: App(),
+      home: PageLogin(),
     );
   }
 }
-
 
 class PageLogin extends StatefulWidget {
   const PageLogin({super.key});
@@ -34,29 +33,80 @@ class _PageLoginState extends State<PageLogin> {
     TextEditingController txtPass = TextEditingController();
 
     return Scaffold(
-      appBar: AppBar(
-          title: const Text("Đăng nhập")
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            TextField(
-              controller: txtUser,
-              decoration: const InputDecoration(
-                labelText: "Tên đăng nhập"
-              ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                space(0, 50),
+                Image.asset("assets/freed.png"),
+                space(0, 50),
+                TextFormField(
+                  controller: txtUser,
+                  decoration: const InputDecoration(
+                      labelText: "Số điện thoại",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.person)),
+                ),
+                space(0, 15),
+                TextFormField(
+                  controller: txtPass,
+                  decoration: InputDecoration(
+                    labelText: "Mật khẩu",
+                    border: const OutlineInputBorder(),
+                    prefixIcon: const Icon(Icons.lock),
+                    suffixIcon:
+                        IconButton(onPressed: () {}, icon: const Icon(Icons.remove_red_eye)),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Quên mật khẩu",
+                      style: TextStyle(fontSize: 15, color: Colors.redAccent),
+                    ),
+                  ),
+                ),
+                space(0, 50),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(55),
+                    backgroundColor: Colors.redAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child:
+                      const Text("Đăng nhập", style: TextStyle(fontSize: 18, color: Colors.white)),
+                ),
+                space(0, 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Bạn không có Tài khoản?",
+                      style: TextStyle(fontSize: 15, color: Colors.black54),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Đăng kí",
+                        style: TextStyle(
+                          color: Colors.redAccent,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
             ),
-
-            space(0, 10),
-            TextField(
-              controller: txtPass,
-              obscureText: showPass,
-              decoration: const InputDecoration(
-                labelText: "Mật khẩu"
-              ),
-            ),
-
-          ],
+          ),
         ),
       ),
     );
