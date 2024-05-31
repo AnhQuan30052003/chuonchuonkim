df3b355abb4598a2778250b74eef1128a895a044
tiendatpy <datn71356@gmail.com>
2024-05-31 15:48:09 +0700

    update

--- SanPhamAndroid/lib/pages/admin/pageOrder.dart
+++ SanPhamAndroid/lib/pages/admin/pageOrder.dart
@@ -1,1 +1,21 @@
-// Quân
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class PageOrder extends StatelessWidget {
+  const PageOrder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white10,
+        title: const Text(
+          "Xác nhận đơn hàng",
+          style: TextStyle(fontSize: 16, color: Color(0xFF3A3737), fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
