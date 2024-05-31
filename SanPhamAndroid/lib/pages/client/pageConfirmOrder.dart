b65298d81dea189a566f9648b253a117eb749320
tiendatpy <datn71356@gmail.com>
2024-05-31 08:40:45 +0700

    update

--- SanPhamAndroid/lib/pages/client/pageConfirmOrder.dart
+++ SanPhamAndroid/lib/pages/client/pageConfirmOrder.dart
@@ -1,27 +1,83 @@
+import 'package:chuonchuonkim_app/controllers/chuonChuonKimController.dart';
+import 'package:chuonchuonkim_app/helper/widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ConfirmOrder(),
+    );
+  }
+}
 
 class ConfirmOrder extends StatelessWidget {
   const ConfirmOrder({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // var c = ChuonChuonKimController.instance.userSnapshot!;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Xác nhận đặt hàng"),
       ),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 10),
           child: Column(
             children: [
-              Text("Địa chỉ nhận hàng"),
-              SizedBox(
-                height: 40,
-                width: double.infinity,
-                child: Row(
-                  children: [],
+              space(0, 20),
+              const Row(
+                children: [
+                  Text(
+                    "Địa chỉ nhận hàng",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              space(0, 20),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(13),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black54,
+                      blurRadius: 3,
+                      spreadRadius: 1,
+                    )
+                  ],
                 ),
-              )
+                height: 60,
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text("Ninh than, Ninh hoa"),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text("Thay đổi", style: TextStyle(color: Colors.red)),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              space(0, 20),
+              const Row(
+                children: [
+                  Text(
+                    "Phương thức thanh toán",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
